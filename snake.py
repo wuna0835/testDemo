edfc1860a5ec70aba51fc4acc1a3dce9e5630be2
wuna161 <44792243+wuna161@users.noreply.github.com>
2019-03-12 12:12:11 +0800

    Add files via upload

---
+++ snake.py
@@ -0,0 +1,41 @@
+how_many_snakes = 1
+snake_string = """
+Welcome to Python3!
+
+             ____
+            / . .\\
+            \  ---<
+             \  /
+   __________/ /
+-=:___________/
+
+<3, Juno
+"""
+
+
+print(snake_string * how_many_snakes)
+print(snake_string * how_many_snakes)
+
+
+
+def create_groups(items, num_groups):
+    try:
+        size = len(items) // num_groups
+    except ZeroDivisionError:
+        print("WARNING: Returning empty list. Please use a nonzero number.")
+        return []
+    else:
+        groups = []
+        for i in range(0, len(items), size):
+            groups.append(items[i:i + size])
+        return groups
+    finally:
+        print("{} groups returned.".format(num_groups))
+
+print("Creating 6 groups...")
+for group in create_groups(range(32), 6):
+    print(list(group))
+
+print("\nCreating 0 groups...")
+for group in create_groups(range(32), 0):
+    print(list(group))
